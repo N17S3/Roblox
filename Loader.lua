eca90a547b63f6397964f543b69656a1843b99c8
Zack <110104519+N17S3@users.noreply.github.com>
2026-06-27 20:57:50 +0200

    N/A

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 local UserInputService = game:GetService("UserInputService")
 local API = {}
 API.DeviceCheck = function()
-	if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
+	if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/N17S3/Roblox/refs/heads/main/Mobile.lua"))();
-	else
+	elseif UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/N17S3/Roblox/refs/heads/main/PC.lua"))();
 	end
 end
